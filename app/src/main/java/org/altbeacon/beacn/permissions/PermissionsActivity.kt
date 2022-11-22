c496943fa74fbe80a81da50b8e5182375235219b
David G. Young <davidgyoung@gmail.com>
2022-11-21 19:34:46 -0500

    Add new permissions screen

---
+++ app/src/main/java/org/altbeacon/beacn/permissions/PermissionsActivity.kt
@@ -0,0 +1,270 @@
+package org.altbeacon.beacon.permissions
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.setPadding
+
+open class PermissionsActivity: AppCompatActivity() {
+
+    val requestPermissionsLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            // Handle Permission granted/rejected
+            permissions.entries.forEach {
+                val permissionName = it.key
+                val isGranted = it.value
+                if (isGranted) {
+                    Log.d(TAG, "$permissionName permission granted: $isGranted")
+                    // Permission is granted. Continue the action or workflow in your
+                    // app.
+                } else {
+                    Log.d(TAG, "$permissionName permission granted: $isGranted")
+                    // Explain to the user that the feature is unavailable because the
+                    // features requires a permission that the user has denied. At the
+                    // same time, respect the user's decision. Don't link to system
+                    // settings in an effort to convince the user to change their
+                    // decision.
+                }
+            }
+        }
+
+    companion object {
+        const val TAG = "PermissionsActivity"
+    }
+}
+
+class PermissionsHelper(val context: Context) {
+    // Manifest.permission.ACCESS_BACKGROUND_LOCATION
+    // Manifest.permission.ACCESS_FINE_LOCATION
+    // Manifest.permission.BLUETOOTH_CONNECT
+    // Manifest.permission.BLUETOOTH_SCAN
+    fun isPermissionGranted(permissionString: String): Boolean {
+        return (ContextCompat.checkSelfPermission(context, permissionString) == PackageManager.PERMISSION_GRANTED)
+    }
+    fun setFirstTimeAskingPermission(permissionString: String, isFirstTime: Boolean) {
+        val sharedPreference = context.getSharedPreferences("org.altbeacon.permisisons",
+            AppCompatActivity.MODE_PRIVATE
+        )
+        sharedPreference.edit().putBoolean(permissionString,
+            isFirstTime).apply()
+    }
+
+    fun isFirstTimeAskingPermission(permissionString: String): Boolean {
+        val sharedPreference = context.getSharedPreferences(
+            "org.altbeacon.permisisons",
+            AppCompatActivity.MODE_PRIVATE
+        )
+        return sharedPreference.getBoolean(
+            permissionString,
+            true
+        )
+    }
+    fun beaconScanPermissionGroupsNeeded(backgroundAccessRequested: Boolean = false): List<Array<String>> {
+        val permissions = ArrayList<Array<String>>()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // As of version M (6) we need FINE_LOCATION (or COARSE_LOCATION, but we ask for FINE)
+            permissions.add(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION))
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            // As of version Q (10) we need FINE_LOCATION and BACKGROUND_LOCATION
+            if (backgroundAccessRequested) {
+                permissions.add(arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION))
+            }
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            // As of version S (12) we need FINE_LOCATION, BLUETOOTH_SCAN and BACKGROUND_LOCATION
+            // Manifest.permission.BLUETOOTH_CONNECT is not absolutely required to do just scanning,
+            // but it is required if you want to access some info from the scans like the device name
+            // and the aditional cost of requsting this access is minimal, so we just request it
+            permissions.add(arrayOf(Manifest.permission.BLUETOOTH_SCAN, Manifest.permission.BLUETOOTH_CONNECT))
+        }
+        return permissions
+    }
+
+}
+
+
+
+open class BeaconScanPermissionsActivity: PermissionsActivity()  {
+    lateinit var layout: LinearLayout
+    lateinit var permissionGroups: List<Array<String>>
+    lateinit var continueButton: Button
+    var scale: Float = 1.0f
+        get() {
+            return this.getResources().getDisplayMetrics().density
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        layout = LinearLayout(this)
+        layout.setPadding(dp(20))
+        layout.gravity = Gravity.CENTER
+        layout.setBackgroundColor(Color.WHITE)
+        layout.orientation = LinearLayout.VERTICAL
+        val backgroundAccessRequested = intent.getBooleanExtra("backgroundAccessRequested", true)
+        val title = intent.getStringExtra("title") ?: "Permissions Needed"
+        val message = intent.getStringExtra("message") ?: "In order to scan for beacons, this app requrires the following permissions from the operating system.  Please tap each button to grant each required permission."
+        val continueButtonTitle = intent.getStringExtra("continueButtonTitle") ?: "Continue"
+        val permissionButtonTitles = intent.getBundleExtra("permissionBundleTitles") ?: getDefaultPermissionTitlesBundle()
+
+        permissionGroups = PermissionsHelper(this).beaconScanPermissionGroupsNeeded(backgroundAccessRequested)
+
+        val params = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        params.setMargins(dp(0), dp(10), dp(0), dp(10))
+
+
+        val titleView = TextView(this)
+        titleView.setGravity(Gravity.CENTER)
+        titleView.textSize = dp(10).toFloat()
+        titleView.text = title
+        titleView.layoutParams = params
+
+        layout.addView(titleView)
+        val messageView = TextView(this)
+        messageView.text = message
+        messageView.setGravity(Gravity.CENTER)
+        messageView.textSize = dp(5).toFloat()
+        messageView.textAlignment = TextView.TEXT_ALIGNMENT_CENTER
+        messageView.layoutParams = params
+        layout.addView(messageView)
+
+        var index = 0
+        for (permissionGroup in permissionGroups) {
+            val button = Button(this)
+            val buttonTitle = permissionButtonTitles.getString(permissionGroup.first())
+            button.id = index
+            button.text = buttonTitle
+            button.layoutParams = params
+            button.setOnClickListener(buttonClickListener)
+            layout.addView(button)
+            index += 1
+        }
+
+        continueButton = Button(this)
+        continueButton.text = continueButtonTitle
+        continueButton.isEnabled = false
+        continueButton.setOnClickListener {
+            this.finish()
+        }
+        continueButton.layoutParams = params
+        layout.addView(continueButton)
+
+        setContentView(layout)
+    }
+
+    fun dp(value: Int): Int {
+        return (value * scale + 0.5f).toInt()
+    }
+
+    val buttonClickListener = View.OnClickListener { button ->
+        val permissionsGroup = permissionGroups.get(button.id)
+        promptForPermissions(permissionsGroup)
+    }
+
+    @SuppressLint("InlinedApi")
+    fun getDefaultPermissionTitlesBundle(): Bundle {
+        val bundle = Bundle()
+        bundle.putString(Manifest.permission.ACCESS_FINE_LOCATION, "Location")
+        bundle.putString(Manifest.permission.ACCESS_BACKGROUND_LOCATION, "Background Location")
+        bundle.putString(Manifest.permission.BLUETOOTH_SCAN, "Bluetooth")
+        return bundle
+    }
+
+
+    fun allPermissionGroupsGranted(): Boolean {
+        for (permissionsGroup in permissionGroups) {
+            if (!allPermissionsGranted(permissionsGroup)) {
+                return false
+            }
+        }
+        return true
+    }
+
+    fun setButtonColors() {
+        var index = 0
+        for (permissionsGroup in this.permissionGroups) {
+            val button = findViewById<Button>(index)
+            if (allPermissionsGranted(permissionsGroup)) {
+                button.setBackgroundColor(Color.parseColor("#448844"))
+            }
+            else {
+                button.setBackgroundColor(Color.RED)
+            }
+            index += 1
+        }
+    }
+    override fun onResume() {
+        super.onResume()
+        setButtonColors()
+        if (allPermissionGroupsGranted()) {
+            continueButton.isEnabled = true
+        }
+    }
+
+    fun promptForPermissions(permissionsGroup: Array<String>) {
+        if (!allPermissionsGranted(permissionsGroup)) {
+            val firstPermission = permissionsGroup.first()
+
+            var showRationale = true
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                showRationale = shouldShowRequestPermissionRationale(firstPermission)
+            }
+            if (showRationale ||  PermissionsHelper(this).isFirstTimeAskingPermission(firstPermission)) {
+                PermissionsHelper(this).setFirstTimeAskingPermission(firstPermission, false)
+                requestPermissionsLauncher.launch(permissionsGroup)
+            }
+            else {
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("Can't request permission")
+                builder.setMessage("This permission has been previously denied to this app.  In order to grant it now, you must go to Android Settings to enable this permission.")
+                builder.setPositiveButton("OK", null)
+                builder.show()
+            }
+        }
+    }
+    fun allPermissionsGranted(permissionsGroup: Array<String>): Boolean {
+        val permissionsHelper = PermissionsHelper(this)
+        for (permission in permissionsGroup) {
+            if (!permissionsHelper.isPermissionGranted(permission)) {
+                return false
+            }
+        }
+        return true
+    }
+
+    companion object {
+        const val TAG = "BeaconScanPermissionActivity"
+        fun allPermissionsGranted(context: Context, backgroundAccessRequested: Boolean): Boolean {
+            val permissionsHelper = PermissionsHelper(context)
+            val permissionsGroups = permissionsHelper.beaconScanPermissionGroupsNeeded(backgroundAccessRequested)
+            for (permissionsGroup in permissionsGroups) {
+                for (permission in permissionsGroup) {
+                    if (!permissionsHelper.isPermissionGranted(permission)) {
+                        return false
+                    }
+                }
+            }
+            return true
+        }
+    }
+}
--- app/src/main/java/org/altbeacon/beaconreference/MainActivity.kt
+++ app/src/main/java/org/altbeacon/beaconreference/MainActivity.kt
@@ -1,9 +1,6 @@
 package org.altbeacon.beaconreference
 
-import android.Manifest
 import android.app.AlertDialog
-import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -16,6 +13,8 @@ import androidx.lifecycle.Observer
 import org.altbeacon.beacon.Beacon
 import org.altbeacon.beacon.BeaconManager
 import org.altbeacon.beacon.MonitorNotifier
+import android.content.Intent
+import org.altbeacon.beacon.permissions.BeaconScanPermissionsActivity
 
 class MainActivity : AppCompatActivity() {
     lateinit var beaconListView: ListView
@@ -24,7 +23,6 @@ class MainActivity : AppCompatActivity() {
     lateinit var rangingButton: Button
     lateinit var beaconReferenceApplication: BeaconReferenceApplication
     var alertDialog: AlertDialog? = null
-    var neverAskAgainPermissions = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,7 +50,22 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         Log.d(TAG, "onResume")
         super.onResume()
-        checkPermissions()
+        // You MUST make sure the following dynamic permissions are granted by the user to detect beacons
+        //
+        //    Manifest.permission.BLUETOOTH_SCAN
+        //    Manifest.permission.BLUETOOTH_CONNECT
+        //    Manifest.permission.ACCESS_FINE_LOCATION
+        //    Manifest.permission.ACCESS_BACKGROUND_LOCATION // only needed to detect in background
+        //
+        // The code needed to get these permissions has become increasingly complex, so it is in
+        // its own file so as not to clutter this file focussed on how to use the library.
+
+        if (!BeaconScanPermissionsActivity.allPermissionsGranted(this,
+                true)) {
+            val intent = Intent(this, BeaconScanPermissionsActivity::class.java)
+            intent.putExtra("backgroundAccessRequested", true)
+            startActivity(intent)
+        }
     }
 
     val monitoringObserver = Observer<Int> { state ->
@@ -131,164 +144,6 @@ class MainActivity : AppCompatActivity() {
         alertDialog?.dismiss()
         alertDialog = builder.create()
         alertDialog?.show()
-
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        for (i in 1..permissions.size-1) {
-            Log.d(TAG, "onRequestPermissionResult for "+permissions[i]+":" +grantResults[i])
-            if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
-                //check if user select "never ask again" when denying any permission
-                if (!shouldShowRequestPermissionRationale(permissions[i])) {
-                    neverAskAgainPermissions.add(permissions[i])
-                }
-            }
-        }
-    }
-
-
-    fun checkPermissions() {
-        // basepermissions are for M and higher
-        var permissions = arrayOf( Manifest.permission.ACCESS_FINE_LOCATION)
-        var permissionRationale ="This app needs fine location permission to detect beacons.  Please grant this now."
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            permissions = arrayOf( Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.BLUETOOTH_SCAN)
-            permissionRationale ="This app needs fine location permission, and bluetooth scan permission to detect beacons.  Please grant all of these now."
-        }
-        else if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            if ((checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
-                permissions = arrayOf( Manifest.permission.ACCESS_FINE_LOCATION)
-                permissionRationale ="This app needs fine location permission to detect beacons.  Please grant this now."
-            }
-            else {
-                permissions = arrayOf( Manifest.permission.ACCESS_BACKGROUND_LOCATION)
-                permissionRationale ="This app needs background location permission to detect beacons in the background.  Please grant this now."
-            }
-        }
-        else if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            permissions = arrayOf( Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_BACKGROUND_LOCATION)
-            permissionRationale ="This app needs both fine location permission and background location permission to detect beacons in the background.  Please grant both now."
-        }
-        var allGranted = true
-        for (permission in permissions) {
-            if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) allGranted = false;
-        }
-        if (!allGranted) {
-            if (neverAskAgainPermissions.count() == 0) {
-                val builder =
-                    AlertDialog.Builder(this)
-                builder.setTitle("This app needs permissions to detect beacons")
-                builder.setMessage(permissionRationale)
-                builder.setPositiveButton(android.R.string.ok, null)
-                builder.setOnDismissListener {
-                    requestPermissions(
-                        permissions,
-                        PERMISSION_REQUEST_FINE_LOCATION
-                    )
-                }
-                builder.show()
-            }
-            else {
-                val builder =
-                    AlertDialog.Builder(this)
-                builder.setTitle("Functionality limited")
-                builder.setMessage("Since location and device permissions have not been granted, this app will not be able to discover beacons.  Please go to Settings -> Applications -> Permissions and grant location and device discovery permissions to this app.")
-                builder.setPositiveButton(android.R.string.ok, null)
-                builder.setOnDismissListener { }
-                builder.show()
-            }
-        }
-        else {
-            if (android.os.Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
-                if (checkSelfPermission(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
-                    != PackageManager.PERMISSION_GRANTED
-                ) {
-                    if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_BACKGROUND_LOCATION)) {
-                        val builder =
-                            AlertDialog.Builder(this)
-                        builder.setTitle("This app needs background location access")
-                        builder.setMessage("Please grant location access so this app can detect beacons in the background.")
-                        builder.setPositiveButton(android.R.string.ok, null)
-                        builder.setOnDismissListener {
-                            requestPermissions(
-                                arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION),
-                                PERMISSION_REQUEST_BACKGROUND_LOCATION
-                            )
-                        }
-                        builder.show()
-                    } else {
-                        val builder =
-                            AlertDialog.Builder(this)
-                        builder.setTitle("Functionality limited")
-                        builder.setMessage("Since background location access has not been granted, this app will not be able to discover beacons in the background.  Please go to Settings -> Applications -> Permissions and grant background location access to this app.")
-                        builder.setPositiveButton(android.R.string.ok, null)
-                        builder.setOnDismissListener { }
-                        builder.show()
-                    }
-                }
-            }
-            else if (android.os.Build.VERSION.SDK_INT > Build.VERSION_CODES.S &&
-                (checkSelfPermission(Manifest.permission.BLUETOOTH_SCAN)
-                        != PackageManager.PERMISSION_GRANTED)) {
-                if (shouldShowRequestPermissionRationale(Manifest.permission.BLUETOOTH_SCAN)) {
-                    val builder =
-                        AlertDialog.Builder(this)
-                    builder.setTitle("This app needs bluetooth scan permission")
-                    builder.setMessage("Please grant scan permission so this app can detect beacons.")
-                    builder.setPositiveButton(android.R.string.ok, null)
-                    builder.setOnDismissListener {
-                        requestPermissions(
-                            arrayOf(Manifest.permission.BLUETOOTH_SCAN),
-                            PERMISSION_REQUEST_BLUETOOTH_SCAN
-                        )
-                    }
-                    builder.show()
-                } else {
-                    val builder =
-                        AlertDialog.Builder(this)
-                    builder.setTitle("Functionality limited")
-                    builder.setMessage("Since bluetooth scan permission has not been granted, this app will not be able to discover beacons  Please go to Settings -> Applications -> Permissions and grant bluetooth scan permission to this app.")
-                    builder.setPositiveButton(android.R.string.ok, null)
-                    builder.setOnDismissListener { }
-                    builder.show()
-                }
-            }
-            else {
-                if (android.os.Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
-                    if (checkSelfPermission(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
-                        != PackageManager.PERMISSION_GRANTED
-                    ) {
-                        if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_BACKGROUND_LOCATION)) {
-                            val builder =
-                                AlertDialog.Builder(this)
-                            builder.setTitle("This app needs background location access")
-                            builder.setMessage("Please grant location access so this app can detect beacons in the background.")
-                            builder.setPositiveButton(android.R.string.ok, null)
-                            builder.setOnDismissListener {
-                                requestPermissions(
-                                    arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION),
-                                    PERMISSION_REQUEST_BACKGROUND_LOCATION
-                                )
-                            }
-                            builder.show()
-                        } else {
-                            val builder =
-                                AlertDialog.Builder(this)
-                            builder.setTitle("Functionality limited")
-                            builder.setMessage("Since background location access has not been granted, this app will not be able to discover beacons in the background.  Please go to Settings -> Applications -> Permissions and grant background location access to this app.")
-                            builder.setPositiveButton(android.R.string.ok, null)
-                            builder.setOnDismissListener { }
-                            builder.show()
-                        }
-                    }
-                }
-            }
-        }
 
     }
 
