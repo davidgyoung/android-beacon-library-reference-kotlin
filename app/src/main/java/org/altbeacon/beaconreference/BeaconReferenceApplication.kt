b757da3ab6d725af6cea07153cb54592892430a5
David G. Young <davidgyoung@gmail.com>
2024-10-08 16:36:13 -0400

    Update reference app to show how new settings api works

--- app/src/main/java/org/altbeacon/beaconreference/BeaconReferenceApplication.kt
+++ app/src/main/java/org/altbeacon/beaconreference/BeaconReferenceApplication.kt
@@ -3,71 +3,66 @@ package org.altbeacon.beaconreference
 import android.app.*
 import android.content.Context
 import android.content.Intent
-import android.os.Handler
-import android.os.Looper
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.lifecycle.Observer
 import com.davidgyoungtech.beaconparsers.IBeaconParser
 import org.altbeacon.beacon.*
-import org.altbeacon.bluetooth.BluetoothMedic
-import java.util.ArrayList
 
 class BeaconReferenceApplication: Application() {
     // the region definition is a wildcard that matches all beacons regardless of identifiers.
     // if you only want to detect beacons with a specific UUID, change the id1 paremeter to
     // a UUID like Identifier.parse("2F234454-CF6D-4A0F-ADF2-F4911BA9FFA6")
-    //var region = Region("all-beacons", null, null, null)
-    val iBeaconRegion = BeaconRegion("all-ibeacons", IBeaconParser(), null, null, null)
-    //val altBeaconRegion = BeaconRegion("all-altbeacons", AltBeaconParser(), null, null, null)
+    // Each BeaconRegion must specify a beacon parser, which defines what format of Bluetooth beacon
+    // to match, e.g. (iBeacon, AltBeacon, Eddystone UID, etc.)
+    // The first parameter uniqueId is a string key that is used to identify this region in the
+    // library.  This string key must be unique for each region you define, and the ame uniqueID
+    // must be used to tell the library to start and stop beacon ranging and monitoring.
+    val wildcardIBeaconRegion = BeaconRegion("everyIBeaconRegion", IBeaconParser(), null, null, null)
+    // Below are examples of other region definitions for other beacon formats
+    //val wildcardAltBeaconRegion = BeaconRegion("everyAltBeaconRegion", AltBeaconParser(), null, null, null)
+    //val everyEddystoneUidBeaconRegion = BeaconRegion("everyEddystoneUidBeaconRegion", AltBeaconParser(), null, null, null)
 
     override fun onCreate() {
         super.onCreate()
-
-        val beaconManager = BeaconManager.getInstanceForApplication(this)
-        BeaconManager.setDebug(true)
-//        beaconManager.beaconParsers.add(BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"))
-
-        // By default the AndroidBeaconLibrary will only find AltBeacons.  If you wish to make it
-        // find a different type of beacon, you must specify the byte layout for that beacon's
-        // advertisement with a line like below.  The example shows how to find a beacon with the
-        // same byte layout as AltBeacon but with a beaconTypeCode of 0xaabb.  To find the proper
-        // layout expression for other beacon types, do a web search for "setBeaconLayout"
-        // including the quotes.
-        //
-        //beaconManager.getBeaconParsers().clear();
-        //beaconManager.getBeaconParsers().add(new BeaconParser().
-        //        setBeaconLayout("m:0-1=4c00,i:2-24v,p:24-24"));
-
-
-        // By default the AndroidBeaconLibrary will only find AltBeacons.  If you wish to make it
-        // find a different type of beacon like Eddystone or iBeacon, you must specify the byte layout
-        // for that beacon's advertisement with a line like below.
-        //
-        // If you don't care about AltBeacon, you can clear it from the defaults:
-        //beaconManager.getBeaconParsers().clear()
-
-        // Uncomment if you want to block the library from updating its distance model database
-        //BeaconManager.setDistanceModelUpdateUrl("")
-
-
-        // enabling debugging will send lots of verbose debug information from the library to Logcat
-        // this is useful for troubleshooting problmes
-        // BeaconManager.setDebug(true)
-
-
-        // The BluetoothMedic code here, if included, will watch for problems with the bluetooth
-        // stack and optionally:
-        // - power cycle bluetooth to recover on bluetooth problems
-        // - periodically do a proactive scan or transmission to verify the bluetooth stack is OK
-        // BluetoothMedic.getInstance().legacyEnablePowerCycleOnFailures(this) // Android 4-12 only
-        // BluetoothMedic.getInstance().enablePeriodicTests(this, BluetoothMedic.SCAN_TEST + BluetoothMedic.TRANSMIT_TEST)
-
         setupBeaconScanning()
     }
     fun setupBeaconScanning() {
         val beaconManager = BeaconManager.getInstanceForApplication(this)
 
+        // The intent scan strategy configured below will set up Blutooth scanning to find beacons
+        // that are delivered by Android Intent.  This works well for background detections.
+        // If you don't need background detectsions, then you don't need to set any settings, and
+        // can accept the default with is to use a service to do the scanning.  This works well for
+        // beacon detections when the app is in the foreground.
+        val settings = Settings(scanStrategy = Settings.IntentScanStrategy())
+        // You can also use the library's built-in "foreground service" to scan for beacons while
+        // the app is in the background using the code below.  This shows a notification to
+        // the user that your app is running , and will require the ACCESS_BACKGROUND_LOCATION
+        // permission to be granted by the user in advance.
+        //val settings = getSettingsForForegroundServiceScanning()
+
+        // This line below will apply the new beacon scanning settings immediately  Any individual
+        // settings not specified on the new settings object will revert to defaults.
+        // If you only want to make a partial change to the settings, without reverting to defaults
+        // for any settings unspecified you may call `beaconManager.adjustSettings(settings)`
+        beaconManager.replaceSettings(settings)
+
+        // The code below will start "monitoring" and "ranging" for beacons matching the region
+        // definition at the top of this file
+        beaconManager.startMonitoring(wildcardIBeaconRegion)
+        beaconManager.startRangingBeacons(wildcardIBeaconRegion)
+
+
+        // Set up a Live Data observer so this Activity can get beacon data from the Application class
+        val regionViewModel = BeaconManager.getInstanceForApplication(this).getRegionViewModel(wildcardIBeaconRegion)
+        // observer will be called each time the monitored regionState changes (inside vs. outside region)
+        regionViewModel.regionState.observeForever( centralMonitoringObserver)
+        // observer will be called each time a new list of beacons is ranged (typically ~1 second in the foreground)
+        regionViewModel.rangedBeacons.observeForever( centralRangingObserver)
+    }
+
+    fun getSettingsForForegroundServiceScanning(): Settings {
 
         val builder = Notification.Builder(this, "BeaconReferenceApp")
         builder.setSmallIcon(R.drawable.ic_launcher_background)
@@ -86,121 +81,25 @@ class BeaconReferenceApplication: Application() {
         builder.setChannelId(channel.id)
         val notification = builder.build()
 
-        val settings = Settings(
+        return Settings(
             scanStrategy = Settings.ForegroundServiceScanStrategy(
                 notification, 456
             ),
-            scanPeriods = Settings.ScanPeriods(1100, 0, 1100, 0),
+            scanPeriods = Settings.ScanPeriods(1100, 0, 1100, 0)
         )
-
-        // This will apply the new settings immediately, overwriting thre previously active settings
-        // any individual settings not specified on the new settings object will be unchanged
-        beaconManager.adjustSettings(settings)
-        //beaconManager.enableForegroundServiceScanning(notification, 456)
-
-        //scanStrategy = Settings.IntentScanStrategy(),
-        //scanPeriods = Settings.ScanPeriods(1100, 0, 1100, 0),
-
-
-        // The code below will start "monitoring" for beacons matching the region definition at the top of this file
-        beaconManager.startMonitoring(iBeaconRegion)
-        beaconManager.startRangingBeacons(iBeaconRegion)
-
-        // These two lines set up a Live Data observer so this Activity can get beacon data from the Application class
-        val regionViewModel = BeaconManager.getInstanceForApplication(this).getRegionViewModel(iBeaconRegion)
-        // observer will be called each time the monitored regionState changes (inside vs. outside region)
-        regionViewModel.regionState.observeForever( centralMonitoringObserver)
-        // observer will be called each time a new list of beacons is ranged (typically ~1 second in the foreground)
-        regionViewModel.rangedBeacons.observeForever( centralRangingObserver)
-
-        testSwitchToIntentScanStrategyFromService()
     }
 
-    fun setupForegroundService() {
-        val builder = Notification.Builder(this, "BeaconReferenceApp")
-        builder.setSmallIcon(R.drawable.ic_launcher_background)
-        builder.setContentTitle("Scanning for Beacons")
-        val intent = Intent(this, MainActivity::class.java)
-        val pendingIntent = PendingIntent.getActivity(
-                        this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT + PendingIntent.FLAG_IMMUTABLE
-                )
-        builder.setContentIntent(pendingIntent);
-        val channel =  NotificationChannel("beacon-ref-notification-id",
-                    "My Notification Name", NotificationManager.IMPORTANCE_DEFAULT)
-        channel.setDescription("My Notification Channel Description")
-        val notificationManager =  getSystemService(
-            Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.createNotificationChannel(channel);
-        builder.setChannelId(channel.getId());
-        Log.d(TAG, "Calling enableForegroundServiceScanning")
-        BeaconManager.getInstanceForApplication(this).enableForegroundServiceScanning(builder.build(), 456);
-        Log.d(TAG, "Back from  enableForegroundServiceScanning")
-    }
-
-    fun testSwitchToIntentScanStrategyFromService() {
-        Handler(Looper.getMainLooper()).postDelayed({
-            // If a different scan strategy was previously enabled
-            // This code will stop any scanning service, and then restart scanning using
-            // the IntentScanStrategy
-            val settingsDelta = Settings(scanStrategy = Settings.IntentScanStrategy())
-            BeaconManager.getInstanceForApplication(this).adjustSettings(settingsDelta)
-        }, 10000000)
-    }
-
-    fun setupBeaconScanningOld() {
-        val beaconManager = BeaconManager.getInstanceForApplication(this)
-
-        // By default, the library will scan in the background every 5 minutes on Android 4-7,
-        // which will be limited to scan jobs scheduled every ~15 minutes on Android 8+
-        // If you want more frequent scanning (requires a foreground service on Android 8+),
-        // configure that here.
-        // If you want to continuously range beacons in the background more often than every 15 mintues,
-        // you can use the library's built-in foreground service to unlock this behavior on Android
-        // 8+.   the method below shows how you set that up.
-        //try {
-        //    setupForegroundService()
-        //}
-        //catch (e: SecurityException) {
-            // On Android TIRAMUSU + this security exception will happen
-            // if location permission has not been granted when we start
-            // a foreground service.  In this case, wait to set this up
-            // until after that permission is granted
-        //    Log.d(TAG, "Not setting up foreground service scanning until location permission granted by user")
-        //    return
-        //}
-
-
-        beaconManager.setEnableScheduledScanJobs(false);
-        beaconManager.setBackgroundBetweenScanPeriod(0);
-        beaconManager.setBackgroundScanPeriod(1100);
-
-        // Ranging callbacks will drop out if no beacons are detected
-        // Monitoring callbacks will be delayed by up to 25 minutes on region exit
-        // beaconManager.setIntentScanningStrategyEnabled(true)
-
-        // The code below will start "monitoring" for beacons matching the region definition at the top of this file
-        beaconManager.startMonitoring(iBeaconRegion)
-        beaconManager.startRangingBeacons(iBeaconRegion)
-        // These two lines set up a Live Data observer so this Activity can get beacon data from the Application class
-        val regionViewModel = BeaconManager.getInstanceForApplication(this).getRegionViewModel(iBeaconRegion)
-        // observer will be called each time the monitored regionState changes (inside vs. outside region)
-        regionViewModel.regionState.observeForever( centralMonitoringObserver)
-        // observer will be called each time a new list of beacons is ranged (typically ~1 second in the foreground)
-        regionViewModel.rangedBeacons.observeForever( centralRangingObserver)
-
-    }
-
-    val centralMonitoringObserver = Observer<Int> { state ->
+    private val centralMonitoringObserver = Observer<Int> { state ->
         if (state == MonitorNotifier.OUTSIDE) {
-            Log.d(TAG, "outside beacon region: "+iBeaconRegion)
+            Log.d(TAG, "outside beacon region.")
         }
         else {
-            Log.d(TAG, "inside beacon region: "+iBeaconRegion)
+            Log.d(TAG, "inside beacon region.")
             sendNotification()
         }
     }
 
-    val centralRangingObserver = Observer<Collection<Beacon>> { beacons ->
+    private val centralRangingObserver = Observer<Collection<Beacon>> { beacons ->
         val rangeAgeMillis = System.currentTimeMillis() - (beacons.firstOrNull()?.lastCycleDetectionTimestamp ?: 0)
         if (rangeAgeMillis < 10000) {
             Log.d(MainActivity.TAG, "Ranged: ${beacons.count()} beacons")
@@ -236,7 +135,7 @@ class BeaconReferenceApplication: Application() {
     }
 
     companion object {
-        val TAG = "BeaconReference"
+        const val TAG = "BeaconReference"
     }
 
 }
